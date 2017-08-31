3029bb6e10abf5010e63f0d2effbbb6d8715f4cb
Julien Mercier <julien.mercier@bnc.ca>
2017-08-31 09:04:00 -0400

    test

--- src/main/java/com/bot/bottest/BotController.java
+++ src/main/java/com/bot/bottest/BotController.java
@@ -106,7 +106,7 @@ public class BotController {
     }
 
     private void sendGenericMessage(String recipientId) {
-       /* var messageData = {
+       /* Message messageData = {
             recipient: {
             id: recipientId
         },
